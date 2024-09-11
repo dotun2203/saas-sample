8286f85b4492b257e661ebcb1f1d475c043d0bae
dotun2203 <adedotunabimbola2@gmail.com>
2024-09-11 15:17:25 +0100

    added env files

--- src/cfchome/settings.py
+++ src/cfchome/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+
+import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m$$j75vfzdpp7%!55n+g3*q9rv1#p4ct*@^2uzf=shj=wkpxz@'
+SECRET_KEY = config("DJANGO_SECRET_KEY", default=None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = os.environ.get("DEBUG") or False
+
+
+DEBUG = config("DJANGO_DEBUG", cast=bool)
+# print("DEBUG ", type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app", ".onrender.com" # http://saas/prod.railway.app
